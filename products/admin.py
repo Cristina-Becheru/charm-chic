9921024c46f82af151f177ef4edd95251a224749
Cristina-Alexandra Becheru <cristina_becheru@yahoo.com>
2024-11-15 12:04:53 +0000

    Customized admin.

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 from .models import Product, Category, SubCategory, Tag
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'category',
+        'name',
+        'sku',
+        'price',
+        'rating',
+        'image',
+    )
+    ordering = ('sku',)
 
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(SubCategory)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+        'category',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(SubCategory, SubCategoryAdmin)
 admin.site.register(Tag)
