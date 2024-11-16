060011c20d8746554966a952e12af2f3c70ab447
Cristina-Alexandra Becheru <cristina_becheru@yahoo.com>
2024-11-16 01:00:24 +0000

    Added Stripe elements.

--- checkout/views.py
+++ checkout/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from .forms import OrderForm
 
+
+
 def checkout(request):
     """
     Display the checkout page and process the order.
     """
+    
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
@@ -15,6 +18,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        
     }
     
     return render(request, template, context)
